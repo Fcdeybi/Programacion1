51f46009ad3186032e7a8f6414b44d4202ffc54e
EduardoBotzoc <159594028+EduardoBotzoc@users.noreply.github.com>
2024-02-17 12:55:47 -0600

    Add files via upload
    Clase 17/02/24

---
+++ EduardoBotzoc.cpp
@@ -0,0 +1,41 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int a, b, c;
+	
+	cout<<"Ingrese dos valores para realizar una suma: " <<endl;
+	cin >> a;
+	cin >> b;
+	
+	c=a+b;
+	
+	cout<< " El resultado de la suma es de: " << c;
+	
+	
+	return 0;
+}
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// run this program using console pauser or add your own getch, system
+
+int main (int argc, char** argv)
+{
+	int a = 4;
+	int b = 1;
+	
+	int suma = a+b;
+	
+	
+	cout << "El resultado de la suma es de: "<<suma<<endl;
+
+return 0;
+
+}
